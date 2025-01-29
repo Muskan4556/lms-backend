22a44a0b98897cbc2614f64a915081a629712900
Muskan4556 <muskan35163@gmail.com>
2025-01-29 13:18:25 +0530

    initial config

---
+++ src/index.ts
@@ -0,0 +1,19 @@
+import express, { Request, Response } from "express";
+import cors from "cors";
+import "dotenv/config";
+const app = express();
+
+// middleware
+app.use(express.json());
+app.use(cors());
+
+// route
+app.get("/health", (req: Request, res: Response) => {
+    res.json({
+      Health: "OK",
+    });
+  });
+
+app.listen(3000, () => {
+  console.log(`Server is running at PORT_NO: 3000: http://localhost:3000/`);
+});
